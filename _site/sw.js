34c6a86e7084551fd845e039b2c48707662ce8f6
expaanse <expaaanse@yandex.com>
2019-03-17 11:29:54 +1100

    Added content

--- _site/sw.js
+++ _site/sw.js
@@ -1,8 +1,8 @@
-const version = '20190317091512';
+const version = '20190317111851';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/general/2018/08/22/hello-world/","/about/","/categories/","/blog/","/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","https://www.gravatar.com/avatar/6c0377abcf4da91cdd35dea4554b2a4c?s=52", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  return ["/general/2018/08/22/hello-world/","/about/","/categories/","/contact/","/blog/","/","/meditation/","/sessions/","/testimonials/","/manifest.json","/assets/search.json","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","https://www.gravatar.com/avatar/6c0377abcf4da91cdd35dea4554b2a4c?s=52", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
   ]
 }
 
---
+++ contact.md
@@ -0,0 +1,15 @@
+---
+layout: page
+title: Contact Peter
+feature_image: "https://picsum.photos/1300/400?image=1057"
+feature_text:
+---
+
+Peter Twigg, Meditation Based Therapies, Healer & Naturopath
+
+Email: [info@petertwigg.com](mailto:info@petertwigg.com)
+
+Practice: Address provided on appointment, Sydney, NSW Australia
+
+Mobile: +61 0450 976 441
+
